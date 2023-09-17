db4a2aa9cdeb044ab567f93d6240f020f7f76d75
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-09-17 15:20:54 +0300

    increment1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 )
 
 const host = "http://localhost:8080"
@@ -26,40 +25,50 @@ func run() {
 	}
 }
 
+func saveUrl(res http.ResponseWriter, req *http.Request) {
+	longURL := req.FormValue("url")
+
+	hash := md5.New()
+
+	_, err := io.WriteString(hash, longURL)
+	if err != nil {
+		return
+	}
+
+	shortCode := base64.URLEncoding.EncodeToString(hash.Sum(nil))[:8]
+
+	shortURL := fmt.Sprintf("%s/%s", host, shortCode)
+
+	urlStore[shortURL] = longURL
+
+	res.WriteHeader(http.StatusCreated)
+
+	_, err = res.Write([]byte(shortURL))
+	if err != nil {
+		return
+	}
+}
+
+func getUrl(res http.ResponseWriter, req *http.Request) {
+	shortURL := fmt.Sprintf("%s%s", host, req.URL.RequestURI())
+
+	url, hasUrl := urlStore[shortURL]
+
+	if hasUrl {
+		res.Header().Set("Location", url)
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	}
+}
+
 func shortenerHandler(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		longURL := req.FormValue("url")
-		hash := md5.New()
-
-		_, err := io.WriteString(hash, longURL)
-		if err != nil {
-			return
-		}
-
-		shortCode := base64.URLEncoding.EncodeToString(hash.Sum(nil))[:8]
-
-		urlStore[shortCode] = longURL
-		shortURL := fmt.Sprintf("%s/%s", host, shortCode)
-
-		_, err = res.Write([]byte(shortURL))
-		if err != nil {
-			return
-		}
-
-		res.WriteHeader(http.StatusCreated)
+		saveUrl(res, req)
 		return
 	}
 
 	if req.Method == http.MethodGet {
-		shortCode := strings.Trim(req.RequestURI, "/")
-		shortURL, hasUrl := urlStore[shortCode]
-
-		if hasUrl {
-			res.Header().Set("Location", shortURL)
-			res.WriteHeader(http.StatusTemporaryRedirect)
-
-			return
-		}
+		getUrl(res, req)
+		return
 	}
 
 	http.Error(res, "Bad Request", http.StatusBadRequest)
