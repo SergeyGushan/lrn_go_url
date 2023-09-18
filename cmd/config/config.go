1e9458bb27d02469550e904e4be30fa587803993
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-09-18 14:06:52 +0300

    increment4

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -7,23 +7,6 @@ type Options struct {
 	B string
 }
 
-func (o Options) String() string {
-	return o.A + ", " + o.B
-}
-
-func (o *Options) Set(s string) error {
-
-	if s == "a" {
-		o.A = s
-	}
-
-	if s == "b" {
-		o.B = s
-	}
-
-	return nil
-}
-
 func Flags() Options {
 	options := Options{}
 	flag.StringVar(&options.A, "a", "localhost:8888", "server address")
