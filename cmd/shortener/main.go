9d06238fa41c2f2158477a72ff0725c378dbdc03
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-10-16 13:50:26 +0300

    правки

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,18 +7,18 @@ import (
 	"net/http"
 )
 
-func main() {
-	config.SetOptions()
-	err := http.ListenAndServe(config.Opt.ServerAddress, URLRouter())
-	if err != nil {
-		return
-	}
-}
-
 func URLRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Post("/", urlhandlers.Save)
 	r.Get("/{shortCode}", urlhandlers.Get)
 
 	return r
+}
+
+func main() {
+	config.SetOptions()
+	err := http.ListenAndServe(config.Opt.ServerAddress, URLRouter())
+	if err != nil {
+		return
+	}
 }
