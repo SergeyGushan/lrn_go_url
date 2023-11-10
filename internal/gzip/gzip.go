487e65beb5580f995b3a3570d18665dcc6e56e8a
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-11-10 18:36:41 +0200

    правки

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -2,44 +2,38 @@ package gzip
 
 import (
 	"compress/gzip"
-	"io"
 	"net/http"
 	"strings"
 )
 
-type gzipWriter struct {
-	http.ResponseWriter
-	Writer io.Writer
-}
-
-func (w gzipWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
-}
-
 func Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Проверяем, поддерживает ли клиент сжатие
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		if err != nil {
-			_, err := io.WriteString(w, err.Error())
-			if err != nil {
-				return
-			}
-			return
-		}
+		w.Header().Set("Content-Encoding", "gzip")
 
+		gz := gzip.NewWriter(w)
 		defer func(gz *gzip.Writer) {
 			err := gz.Close()
 			if err != nil {
-				return
+				panic(err)
 			}
 		}(gz)
 
-		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+		gzWriter := gzipResponseWriter{ResponseWriter: w, Writer: gz}
+		next.ServeHTTP(gzWriter, r)
 	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	*gzip.Writer
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
 }
