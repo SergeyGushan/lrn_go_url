ee1e6c358a62fd77ab45c93922c5d6728b926bd7
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-11-13 23:59:17 +0200

    правки

--- internal/urlhandlers/url_handlers.go
+++ internal/urlhandlers/url_handlers.go
@@ -28,13 +28,18 @@ type StructRes struct {
 func Save(res http.ResponseWriter, req *http.Request) {
 	var bodyReader io.Reader = req.Body
 
-	if !strings.Contains(req.Header.Get("Content-Encoding"), "gzip") {
+	if strings.Contains(req.Header.Get("Content-Encoding"), "gzip") {
 		reader, err := gzip.NewReader(req.Body)
 		if err != nil {
 			http.Error(res, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
-		defer reader.Close()
+		defer func(reader *gzip.Reader) {
+			err := reader.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(reader)
 		bodyReader = reader
 	}
 
