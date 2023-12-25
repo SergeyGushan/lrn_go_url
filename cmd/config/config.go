44624a76d5aa86105ad5ab901fcf4b9e6e84fb91
SergeyGushan <59620046+SergeyGushan@users.noreply.github.com>
2023-12-25 18:04:42 +0200

    iter12

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"flag"
-	"fmt"
 	"github.com/caarlos0/env/v6"
 	"log"
 	"os"
@@ -20,9 +19,9 @@ var Opt = Options{}
 func SetOptions() {
 	flag.StringVar(&Opt.ServerAddress, "a", "localhost:8080", "server address")
 	flag.StringVar(&Opt.BaseURL, "b", "http://localhost:8080", "base url")
-	//flag.StringVar(&Opt.DatabaseDSN, "d", "", "db dsn")
+	flag.StringVar(&Opt.DatabaseDSN, "d", "", "db dsn")
 	flag.StringVar(&Opt.FileStoragePath, "f", os.TempDir()+"/short-url-db.json", "base url")
-	flag.StringVar(&Opt.DatabaseDSN, "d", fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", `localhost`, `go_user`, `go_password`, `go_learn`), "db dsn")
+	//flag.StringVar(&Opt.DatabaseDSN, "d", fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", `localhost`, `go_user`, `go_password`, `go_learn`), "db dsn")
 	flag.Parse()
 
 	err := env.Parse(&Opt)
